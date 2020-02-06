d32123fe2cf93506c72d925dfe1710568a671600
Shaun Gedye <sgedye88@yahoo.com.au>
2020-02-06 12:11:53 +1100

    Updates

---
+++ precache-manifest.cca8abb5ae714fc4f70f0917aa2c9e92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68edf10755e363e3319510c72347b65",
+    "url": "/mini-projects/index.html"
+  },
+  {
+    "revision": "53e8a1c836d6869382df",
+    "url": "/mini-projects/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3b22f9af38613738be7",
+    "url": "/mini-projects/static/js/2.1d401f9a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mini-projects/static/js/2.1d401f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e8a1c836d6869382df",
+    "url": "/mini-projects/static/js/main.a46c513f.chunk.js"
+  },
+  {
+    "revision": "42ad75350cc7b6b3afdc",
+    "url": "/mini-projects/static/js/runtime-main.036eb54d.js"
+  },
+  {
+    "revision": "aff7043c0ae20a1a947732ba028f1505",
+    "url": "/mini-projects/static/media/birds.aff7043c.jpg"
+  },
+  {
+    "revision": "0476be31e0019b58402ae6ed937dc926",
+    "url": "/mini-projects/static/media/color.0476be31.png"
+  },
+  {
+    "revision": "825d26619c97daaecf77fc37848099de",
+    "url": "/mini-projects/static/media/counter.825d2661.png"
+  },
+  {
+    "revision": "60454a84d3b59c293d2968f5fe1532a5",
+    "url": "/mini-projects/static/media/quotes.60454a84.png"
+  },
+  {
+    "revision": "4e01398c78a0fd49e21bd3f23c317519",
+    "url": "/mini-projects/static/media/svg-writing.4e01398c.png"
+  }
+]);
