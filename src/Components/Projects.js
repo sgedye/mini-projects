857e6d8e40c0e6ea9ec2ad31fb829077078ab124
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-06 17:01:41 +0800

    Added Clock Project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,6 +5,7 @@ import Project from './Project'
 
 import budget from '../imgs/budget.png'
 import calculator from '../imgs/calculator.png'
+import clock from '../imgs/clock.png'
 import color from '../imgs/color.png'
 import counter from '../imgs/counter.png'
 import drumKit from '../imgs/drum-kit.png'
@@ -30,13 +31,14 @@ function Projects() {
     { id: 1, title: "Color Picker", img: `${color}`, alt: "Colour Picker", desc: "JavaScript, HTML, SCSS", address: "color-picker" },
     { id: 2, title: "Animal Selector", img: `${selector}`, alt: "jQuery Animal Selector", desc: "jQuery, HTML, Bootstrap, SCSS", address: "selector" },
     { id: 3, title: "Quote Machine", img: `${quotes}`, alt: "Quote Machine", desc: "ReactJS, HTML, SCSS", address: "quotes" },
-    { id: 4, title: "Desktop Slideshow", img: `${slideshow}`, alt: "Desktop Slideshow", desc: "JavaScript, HTML, CSS", address: "slideshow" },
+    { id: 4, title: "Drum Machine", img: `${drumKit}`, alt: "Drum Machine", desc: "ReactJS, Styled Components, Hooks, HTML, CSS, MouseTrap", address: "drum-machine" },
     { id: 5, title: "React Calculator", img: `${calculator}`, alt: "React Calculator", desc: "ReactJS, Styled Components, HTML, SCSS, CSS, MouseTrap", address: "calculator" },
     { id: 6, title: "Weather App", img: `${weather}`, alt: "Weather App", desc: "APIs, JSON, JavaScript, HTML, CSS, Skycons", address: "weather" },
     { id: 7, title: "Budget App", img: `${budget}`, alt: "Budget Application", desc: "JavaScript, HTML, Bootstrap, CSS", address: "budget-app" },
-    { id: 8, title: "Drum Machine", img: `${drumKit}`, alt: "Drum Machine", desc: "ReactJS, Styled Components, Hooks, HTML, CSS, MouseTrap", address: "drum-machine" },
-    { id: 9, title: "React Counter", img: `${counter}`, alt: "React Counter", desc: "ReactJS, HTML, CSS", address: "counter" },
-    { id: 10, title: "SVG Writing", img: `${svgWriting}`, alt: "SVG Writing", desc: "HTML, CSS", address: "svg-writing" },
+    { id: 8, title: "Clock", img: `${clock}`, alt: "CSS & JS Clock", desc: "JavaScript, HTML, CSS", address: "clock" },
+    { id: 9, title: "Desktop Slideshow", img: `${slideshow}`, alt: "Desktop Slideshow", desc: "JavaScript, HTML, CSS", address: "slideshow" },
+    { id: 10, title: "React Counter", img: `${counter}`, alt: "React Counter", desc: "ReactJS, HTML, CSS", address: "counter" },
+    { id: 11, title: "SVG Writing", img: `${svgWriting}`, alt: "SVG Writing", desc: "HTML, CSS", address: "svg-writing" },
   ]
   const PROJECT_LIST = PROJECTS.map(project => {
     return (
