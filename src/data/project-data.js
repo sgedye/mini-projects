d78271288968ada8f238b9773dcc639008fa98f9
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-26 21:00:01 +0800

    :rocket: Added Contact-Form Project :rocket:

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,8 +1,9 @@
 import budget from '../imgs/budget.png'
 import calculator from '../imgs/calculator.png'
 import clock from '../imgs/clock.png'
+import contactForm from '../imgs/contact-form.png'
 import color from '../imgs/color.png'
-import counter from '../imgs/counter.png'
+// import counter from '../imgs/counter.png'
 import dragDrop from '../imgs/drag-n-drop.png'
 import drumKit from '../imgs/drum-kit.png'
 import fakeGoogle from '../imgs/fake-google.png'
@@ -19,17 +20,18 @@ export default [
   { id: 0, title: "Meme Builder",     img: meme,      alt: "Meme Builder",      desc: "React, Styled Components, ReactIcons, API, HTML, CSS", onGitHub: true, address: "meme-builder" },
   { id: 1, title: "Color Picker",     img: color,     alt: "Colour Picker",     desc: "JavaScript, HTML, SCSS", onGitHub: true, address: "color-picker" },
   { id: 2, title: "Animal Selector",  img: selector,  alt: "jQuery Animal Selector", desc: "jQuery, HTML, Bootstrap, SCSS", onGitHub: true, address: "selector" },
-  { id: 3, title: "Drum Machine",     img: drumKit,   alt: "Drum Machine",      desc: "React, Styled Components, Hooks, HTML, CSS, MouseTrap", onGitHub: true, address: "drum-machine" },
-  { id: 4, title: "React Calculator", img: calculator, alt: "React Calculator", desc: "React, Styled Components, HTML, SCSS, CSS, MouseTrap", onGitHub: true, address: "calculator" },
-  { id: 5, title: "Weather App",      img: weather,   alt: "Weather App",       desc: "APIs, JSON, JavaScript, HTML, CSS, Skycons", onGitHub: true, address: "weather" },
-  { id: 6, title: "Pomodoro Timer",   img: pomodoro,  alt: "Pomodoro Timer",    desc: "React, Hooks, Styled-Components, React-Icons, CSS", onGitHub: true, address: "pomodoro" },
-  { id: 7, title: "Fake Google",      img: fakeGoogle, alt: "Fake Google",      desc: "React, SCSS, Netlify, SVG, Sprites, HTML", onGitHub: false, address: ["fake-google", "https://fake-google.netlify.app/"] },
-  { id: 8, title: "Budget App",       img: budget,    alt: "Budget Application", desc: "JavaScript, HTML, Bootstrap, CSS", onGitHub: true, address: "budget-app" },
-  { id: 9, title: "Clock",            img: clock,     alt: "CSS & JS Clock",    desc: "JavaScript, HTML, CSS", onGitHub: true, address: "clock" },
-  { id: 10, title: "Grandmas Sweets", img: grandmas,  alt: "Grandmas Sweets",   desc: "HTML, CSS, JavaScript", onGitHub: true, address: "grandmas-sweets" },
-  { id: 11, title: "Slideshow",       img: slideshow, alt: "Slideshow with Autoplay", desc: "JavaScript, HTML, CSS", onGitHub: true, address: "slideshow" },
-  { id: 12, title: "Drag 'n' Drop",   img: dragDrop,  alt: "Drag 'n' Drop",     desc: "React, HTML, CSS", onGitHub: true, address: "drag-n-drop" },
-  { id: 13, title: "Quote Machine",   img: quotes,    alt: "Quote Machine",     desc: "React, HTML, SCSS", onGitHub: true, address: "quotes" },
+  { id: 3, title: "Pomodoro Timer",   img: pomodoro,  alt: "Pomodoro Timer",    desc: "React, Hooks, Styled-Components, React-Icons, CSS", onGitHub: true, address: "pomodoro" },
+  { id: 4, title: "Drum Machine",     img: drumKit,   alt: "Drum Machine",      desc: "React, Styled Components, Hooks, HTML, CSS, MouseTrap", onGitHub: true, address: "drum-machine" },
+  { id: 5, title: "React Calculator", img: calculator, alt: "React Calculator", desc: "React, Styled Components, HTML, SCSS, CSS, MouseTrap", onGitHub: true, address: "calculator" },
+  { id: 6, title: "Weather App",      img: weather,   alt: "Weather App",       desc: "APIs, JSON, JavaScript, HTML, CSS, Skycons", onGitHub: true, address: "weather" },
+  { id: 7, title: "Contact Form",     img: contactForm, alt: "Contact Form",    desc: "React, SCSS Modules, Regex, Hooks, HTML, CSS", onGitHub: false, address: ["contact-form", "https://contact-component.netlify.app/"] },
+  { id: 8, title: "Fake Google",      img: fakeGoogle, alt: "Fake Google",      desc: "React, SCSS, Netlify, SVG, Sprites, HTML", onGitHub: false, address: ["fake-google", "https://fake-google.netlify.app/"] },
+  { id: 9, title: "Grandmas Sweets", img: grandmas,  alt: "Grandmas Sweets",   desc: "HTML, CSS, JavaScript", onGitHub: true, address: "grandmas-sweets" },
+  { id: 10, title: "Clock",            img: clock,     alt: "CSS & JS Clock",    desc: "JavaScript, HTML, CSS", onGitHub: true, address: "clock" },
+  { id: 11, title: "Budget App",       img: budget,    alt: "Budget Application", desc: "JavaScript, HTML, Bootstrap, CSS", onGitHub: true, address: "budget-app" },
+  { id: 12, title: "Slideshow",       img: slideshow, alt: "Slideshow with Autoplay", desc: "JavaScript, HTML, CSS", onGitHub: true, address: "slideshow" },
+  { id: 13, title: "Drag 'n' Drop",   img: dragDrop,  alt: "Drag 'n' Drop",     desc: "React, HTML, CSS", onGitHub: true, address: "drag-n-drop" },
   { id: 14, title: "SVG Writing",     img: svgWriting, alt: "SVG Writing",      desc: "HTML, CSS", onGitHub: true, address: "svg-writing" },
-  { id: 15, title: "React Counter",   img: counter,   alt: "React Counter",     desc: "React, HTML, CSS", onGitHub: true, address: "counter" },
+  { id: 15, title: "Quote Machine",   img: quotes,    alt: "Quote Machine",     desc: "React, HTML, SCSS", onGitHub: true, address: "quotes" },
+  // { id: 16, title: "React Counter",   img: counter,   alt: "React Counter",     desc: "React, HTML, CSS", onGitHub: true, address: "counter" },
 ];
